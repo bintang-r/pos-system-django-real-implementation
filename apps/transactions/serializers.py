8b44374f48cae5758e5326a977541dd2fed67d15
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-04-02 22:58:05 +0800

    add: scheme meta data (serializers)

--- apps/transactions/serializers.py
+++ apps/transactions/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 from .models import Transaction, TransactionItem
 
-class ItemSerializer(serializers.ModelSerializer):
+class TransactionItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = TransactionItem
-        fields = ['product', 'quantity']
+        fields = ['product', 'quantity', 'subtotal']
+        read_only_fields = ['subtotal']
 
 class TransactionSerializer(serializers.ModelSerializer):
-    items = ItemSerializer(many=True)
+    items = TransactionItemSerializer(many=True)
 
     class Meta:
         model = Transaction
-        fields = ['id', 'total_price', 'items', 'created_at']
+        fields = ['id', 'user', 'total_price', 'items', 'created_at']
+        read_only_fields = ['total_price', 'user']
