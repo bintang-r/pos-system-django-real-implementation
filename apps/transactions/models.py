0e32d469798877971a10f51e61da617c2869f385
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-04-02 11:54:05 +0800

    add: transaction models database

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from apps.products.models import Product
+from django.conf import settings
 
-# Create your models here.
+class Transaction(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class TransactionItem(models.Model):
+    transaction = models.ForeignKey(Transaction, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    subtotal = models.DecimalField(max_digits=12, decimal_places=2)
