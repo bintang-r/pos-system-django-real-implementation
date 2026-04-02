d87acb137a56c5d8bde39fcbb964dfa03f812f65
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-04-02 22:59:44 +0800

    add: bussiness logic controller/views data

--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,13 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .services import POSService
 from .serializers import TransactionSerializer
 
 class CheckoutView(APIView):
     def post(self, request):
-        # Logika pemanggilan service
+        items_data = request.data.get('items', [])
+        if not items_data:
+            return Response({"error": "Keranjang kosong"}, status=status.HTTP_400_BAD_REQUEST)
+
         try:
-            sale = POSService.create_sale(request.user, request.data['items'])
-            return Response({"message": "Transaksi Sukses", "id": sale.id})
+            # Memanggil logic dari Service Layer yang sudah kita buat sebelumnya
+            sale = POSService.create_sale(request.user, items_data)
+            serializer = TransactionSerializer(sale)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Exception as e:
-            return Response({"error": str(e)}, status=400)
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
